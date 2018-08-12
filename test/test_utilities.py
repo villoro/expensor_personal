b79c2da26849e13517f6e25c1258970979a1fb3b
Villoro <villoro7@gmail.com>
2018-08-12 11:46:05 +0200

    [TST] test dfs utilities

--- test/test_utilities.py
+++ test/test_utilities.py
@@ -38,22 +38,71 @@ class TestUtilities(unittest.TestCase):
         """ Test that is able to retrive a valid excel for transactions """
 
         dbx = u.io.get_dropbox_conector()
-
         df = u.io.get_df_transactions(dbx)
 
         # Check that all needed columns are present
         self.assertTrue(all([x in df.columns for x in c.cols.REPLACES_DF_TRANS]))
 
 
-    def test_get_data_without_transactions(self):
+    def test_get_data_without_trans(self):
         """ Test that is able to retrive dataframes from data.xlsx """
 
         dbx = u.io.get_dropbox_conector()
-
         dfs = u.io.get_data_without_transactions(dbx)
 
         # Check that all needed dataframes are present
         self.assertTrue(all([x in dfs for x in c.dfs.ALL_FROM_DATA]))
+
+
+    # ------------------------------ dfs -----------------------------------------------------------
+    def test_fix_df_trans(self):
+        """ Test that is able to retrive dataframes from data.xlsx """
+
+        dbx = u.io.get_dropbox_conector()
+        df = u.io.get_df_transactions(dbx)
+        df = u.dfs.fix_df_trans(df)
+
+        # Check that all needed columns are present
+        self.assertTrue(all([x in df.columns for x in c.cols.DF_TRANS]))
+
+        # Check that expenses and income are positive
+        for x in [c.names.EXPENSES, c.names.INCOMES]:
+            self.assertTrue(df[df[c.cols.TYPE] == x][c.cols.AMOUNT].min() > 0)
+
+
+    def test_filter_data(self):
+        """ Test that is able to filter by some column """
+
+        dbx = u.io.get_dropbox_conector()
+        df = u.io.get_df_transactions(dbx)
+        df = u.dfs.fix_df_trans(df)
+
+        # No filter no modifications
+        self.assertEqual(df.shape, u.dfs.filter_data(df).shape)
+
+        # Check that is able to filter using some personal categories
+        aux = u.dfs.filter_data(df, "Menjar")[c.cols.CATEGORY].unique().tolist()
+        self.assertEqual(aux, ["Menjar"])
+
+        aux = u.dfs.filter_data(df, ["Transport", "Menjar"])[c.cols.CATEGORY].unique().tolist()
+        self.assertEqual(sorted(aux), sorted(["Transport", "Menjar"]))
+
+
+    def test_groupby(self):
+        """ Test that is able group by timewindow """
+
+        dbx = u.io.get_dropbox_conector()
+        df = u.io.get_df_transactions(dbx)
+        df = u.dfs.fix_df_trans(df)
+
+        # When filtered by month, all dates are from day 1
+        aux = u.dfs.group_df_by(df, "M")
+        self.assertEqual(aux.index.strftime("%d").tolist(), ['01']*aux.shape[0])
+
+        # When filtered by year index are ints
+        aux = u.dfs.group_df_by(df, "Y")
+        self.assertEqual(aux.index.min(), df[c.cols.YEAR].min())
+        self.assertEqual(aux.index.max(), df[c.cols.YEAR].max())
 
 
 
