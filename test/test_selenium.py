782742c1c3dcecefd3cabc1fcd62a24e9bc981ce
Villoro <villoro7@gmail.com>
2018-08-15 18:23:19 +0200

    [TST] add app_dashboard selenium tests + fix some errors in test about var names

--- test/test_selenium.py
+++ test/test_selenium.py
@@ -24,10 +24,10 @@ class TestApp(unittest.TestCase):
 
             # Check that there is a sidebar and a body
             self.driver.find_element_by_id("sidebar")
-            self.driver.find_element_by_id("page-content")
+            self.driver.find_element_by_id("body")
 
 
-    def check_one_page(self, page_link, elements):
+    def _check_one_page(self, page_link, elements):
         """
             Check the existence of some elements in a page
 
@@ -44,10 +44,12 @@ class TestApp(unittest.TestCase):
             self.driver.find_element_by_id(elem)
 
 
-    def test_page_upload(self):
-        """ Test the content of page upload """
+    def test_page_dashboard(self):
+        """ Test the content of page heatmaps """
 
-        self.check_one_page(c.dash.LINK_UPLOAD, ["upload_button", "upload_results"])
+        body_elem = ["plot_dash_evol", "plot_dash_l_vs_e", "plot_dash_liq_months"]
+        sidebar_elem = ["slider_dash_rolling_avg"]
+        self._check_one_page(c.dash.LINK_HEATMAPS, body_elem + sidebar_elem)
 
 
     def test_page_evolution(self):
@@ -55,7 +57,7 @@ class TestApp(unittest.TestCase):
 
         body_elem = ["plot_evol", "radio_evol_type", "drop_evol_categ", "radio_evol_tw"]
         sidebar_elem = ["drop_evol_categ", "radio_evol_tw"]
-        self.check_one_page(c.dash.LINK_EVOLUTION, body_elem + sidebar_elem)
+        self._check_one_page(c.dash.LINK_EVOLUTION, body_elem + sidebar_elem)
 
 
     def test_page_comparison(self):
@@ -63,7 +65,7 @@ class TestApp(unittest.TestCase):
 
         body_elem = ["plot_comp_1", "radio_comp_1", "plot_comp_2", "radio_comp_2"]
         sidebar_elem = ["drop_comp_categ"] #, "slider_comp_rolling_avg"]
-        self.check_one_page(c.dash.LINK_COMPARISON, body_elem + sidebar_elem)
+        self._check_one_page(c.dash.LINK_COMPARISON, body_elem + sidebar_elem)
 
 
     def test_page_heatmaps(self):
@@ -71,7 +73,7 @@ class TestApp(unittest.TestCase):
 
         body_elem = ["plot_heat_i", "plot_heat_e", "plot_heat_distribution"]
         sidebar_elem = ["drop_heat_categ"]
-        self.check_one_page(c.dash.LINK_HEATMAPS, body_elem + sidebar_elem)
+        self._check_one_page(c.dash.LINK_HEATMAPS, body_elem + sidebar_elem)
 
 
     def test_page_pies(self):
@@ -79,7 +81,7 @@ class TestApp(unittest.TestCase):
 
         body_elem = ["drop_pie_1"] # "drop_pie_2", "plot_pie_1", "plot_pie_2"
         sidebar_elem = ["drop_pie_categ"]
-        self.check_one_page(c.dash.LINK_PIES, body_elem + sidebar_elem)
+        self._check_one_page(c.dash.LINK_PIES, body_elem + sidebar_elem)
 
 
 
