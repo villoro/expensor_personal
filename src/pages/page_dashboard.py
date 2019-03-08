380966d8fab16adfe29dca2cd20012121544d440
Villoro <villoro7@gmail.com>
2019-03-08 12:58:16 +0100

    [REF] update pages dashboard and comparison to multi-output + fix tests

--- src/pages/page_dashboard.py
+++ src/pages/page_dashboard.py
@@ -24,61 +24,34 @@ class Page(lay.AppPage):
     def __init__(self, app):
         super().__init__({c.dash.INPUT_SMOOTHING: True})
 
-        @app.callback(Output("plot_dash_evol", "figure"), [Input("input_smoothing", "value")])
-        # pylint: disable=unused-variable,unused-argument
-        def update_timeserie_plot(avg_month):
-            """
-                Updates the timeserie plot
-
-                Args:
-                    avg_month:  month to use in time average
-            """
-            return plt_ev.plot_timeserie(
-                DFS[c.dfs.TRANS], avg_month=avg_month, height=self.plot_height
-            )
-
         @app.callback(
-            Output("plot_dash_total_worth", "figure"), [Input("input_smoothing", "value")]
+            [
+                Output(f"plot_dash_{x}", "figure")
+                for x in ["evol", "total_worth", "l_vs_e", "liq_months"]
+            ],
+            [Input("input_smoothing", "value")],
         )
         # pylint: disable=unused-variable,unused-argument
-        def update_plot_total_worth(avg_month):
+        def update_plots(avg_month):
             """
-                Updates the timeserie gradient plot
+                Updates the plots
 
                 Args:
                     avg_month:  month to use in time average
             """
-
-            return plt_inv.total_worth_plot(
-                DFS[c.dfs.LIQUID], DFS[c.dfs.WORTH], avg_month, height=self.plot_height
-            )
-
-        @app.callback(Output("plot_dash_l_vs_e", "figure"), [Input("input_smoothing", "value")])
-        # pylint: disable=unused-variable,unused-argument
-        def update_liquid_vs_expenses(avg_month):
-            """
-                Updates the liquid vs expenses plot
-
-                Args:
-                    avg_month:  month to use in time average
-            """
-
-            return plt_li.plot_expenses_vs_liquid(
-                DFS[c.dfs.LIQUID], DFS[c.dfs.TRANS], avg_month, False, height=self.plot_height
-            )
-
-        @app.callback(Output("plot_dash_liq_months", "figure"), [Input("input_smoothing", "value")])
-        # pylint: disable=unused-variable,unused-argument
-        def update_liquid_months(avg_month):
-            """
-                Updates the survival months plot
-
-                Args:
-                    avg_month:  month to use in time average
-            """
-
-            return plt_li.plot_months(
-                DFS[c.dfs.LIQUID], DFS[c.dfs.TRANS], avg_month, False, height=self.plot_height
+            return (
+                plt_ev.plot_timeserie(
+                    DFS[c.dfs.TRANS], avg_month=avg_month, height=self.plot_height
+                ),
+                plt_inv.total_worth_plot(
+                    DFS[c.dfs.LIQUID], DFS[c.dfs.WORTH], avg_month, height=self.plot_height
+                ),
+                plt_li.plot_expenses_vs_liquid(
+                    DFS[c.dfs.LIQUID], DFS[c.dfs.TRANS], avg_month, False, height=self.plot_height
+                ),
+                plt_li.plot_months(
+                    DFS[c.dfs.LIQUID], DFS[c.dfs.TRANS], avg_month, False, height=self.plot_height
+                ),
             )
 
     def get_body(self):
