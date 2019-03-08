2848caf02d2479512415128c8436ce63f7e33033
Villoro <villoro7@gmail.com>
2019-03-08 12:35:26 +0100

    [FIX] problem when displaying filters

--- src/index.py
+++ src/index.py
@@ -3,7 +3,7 @@
 """
 
 from dash import callback_context
-from dash.dependencies import Input, Output, State
+from dash.dependencies import Input, Output
 
 from pages import get_pages
 from dash_app import create_dash_app
@@ -18,10 +18,7 @@ SERVER = APP.server
 PAGES = get_pages(APP)
 
 
-@APP.callback(
-    [Output("body", "children"), Output("filters", "children")],
-    [Input("url", "pathname"), Input("sync", "n_clicks")],
-)
+@APP.callback(Output("body", "children"), [Input("url", "pathname"), Input("sync", "n_clicks")])
 # pylint: disable=unused-variable
 def change_page(pathname, _):
     """ Updates the page content """
@@ -32,24 +29,27 @@ def change_page(pathname, _):
     if ctx.triggered and ctx.triggered[0]["prop_id"] == "sync.n_clicks":
         sync()
 
-    page = PAGES.get(pathname, None)
-
-    if page is not None:
-        return page.get_body_html(), page.get_filters()
-    return ["404"] * 2
+    if pathname in PAGES:
+        return PAGES[pathname].get_body_html()
+    return "404"
 
 
-@APP.callback(
-    Output("filters-container", "is_open"),
-    [Input("filters-button", "n_clicks")],
-    [State("filters-container", "is_open")],
-)
-def toggle_filters(count, is_open):
+@APP.callback(Output("filters", "children"), [Input("url", "pathname")])
+# pylint: disable=unused-variable
+def display_filters(pathname):
+    """ Updates content based on current page """
+
+    if pathname in PAGES:
+        return PAGES[pathname].get_filters()
+    return "404"
+
+
+@APP.callback(Output("filters-container", "is_open"), [Input("filters-button", "n_clicks")])
+def toggle_filters(count):
     """ hides/opens the filter block """
 
-    if count:
-        return not is_open
-    return is_open
+    # Toggle between show and hide
+    return count % 2 == 1 if count is not None else False
 
 
 if __name__ == "__main__":
